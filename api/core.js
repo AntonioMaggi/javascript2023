e4be8edb957d47297310c4e06acb05c69a63facc
AntonioMaggi <maggia1013@gmail.com>
2023-12-01 15:37:01 +0100

    debugged edit and quill problems

--- api/core.js
+++ api/core.js
@@ -1,111 +1,116 @@
-// get all core functions and methods
+// API endpoint for posts
 const apiUrl = "http://localhost:3000/posts";
+// Reference to the HTML element with the id "blog"
 const blog = document.getElementById("blog");
 
-// Get all posts - services...
-const getLocalApi = () => {
-    return fetch(apiUrl)
-        .then(response => response.json())
-}
+// Function to fetch posts from the local API
+const getLocalApi = () => fetch(apiUrl).then(response => response.json());
 
-// Display all posts
-const displayData = () => {
-    getLocalApi()
-        .then(data => {
-            const sortedData = data.sort((a, b) => b.id - a.id); // Sort by id
-            blog.innerHTML = ""; // Clear existing content
+// Function to display posts on the webpage
+const displayData = () =>
+  getLocalApi().then(data => {
+    // Sort posts by ID in descending order
+    const sortedData = data.sort((a, b) => b.id - a.id);
+    // Clear existing content in the blog element
+    blog.innerHTML = "";
 
-            sortedData.forEach(post => {
-                const card = document.createElement("div");
-                card.className = "card mb-2";
-                card.style = "width: 18rem;";
+    // Iterate through each post and create a card for display
+    sortedData.forEach(post => {
+      const card = document.createElement("div");
+      card.className = "card mb-2";
+      card.style = "width: 18rem;";
 
-                card.innerHTML = `
-                    <div class="card-body">
-                        <h5 class="card-title">${post.title} ${post.id}</h5>
-                        <p class="card-text">${post.body}</p>
-                        <p class="card-text"><small class="text-muted">${post.author}</small></p>
-                        <button class="btn btn-primary" data-id="${post.id}" onclick="editPost(${post.id})">Edit</button>
-                        <button class="btn btn-danger" data-id="${post.id}" onclick="deletePost(${post.id})">Delete</button>
-                    </div>
-                `;
+      card.innerHTML = `
+          <div class="card-body">
+              <h5 class="card-title">${post.title} ${post.id}</h5>
+              <p class="card-text">${post.body}</p>
+              <p class="card-text"><small class="text-muted">${post.author}</small></p>
+              <button class="btn btn-primary" data-id="${post.id}" onclick="editPost(${post.id})">Edit</button>
+              <button class="btn btn-danger" data-id="${post.id}" onclick="deletePost(${post.id})">Delete</button>
+          </div>
+      `;
 
-                blog.appendChild(card);
-            });
-        });
-}
+      blog.appendChild(card);
+    });
+  });
 
-// Post data to server extraced from method becuz of service concept
-const postLocalApi = (postData) => {
-    return fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-    })
-        .then(response => response.json())
-        .then(data => data);
-}
+// Function to post data to the local API
+const postLocalApi = postData =>
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+  }).then(response => response.json());
 
-// Get data from form
+// Function to handle form submission
 const submitData = () => {
-    const title = document.getElementById("title").value;
-    const body = quill.getText();
-    const author = document.getElementById("author").value;
+  // Get values from form elements
+  const title = document.getElementById("title").value;
+  const body = quill.root.innerHTML;
+  const author = document.getElementById("author").value;
 
-    if (title === "" || body === "" || author === "") {
-        alert("Please fill all the fields");
-        return;
+  // Check if any field is empty
+  if (title === "" || body === "" || author === "") {
+    alert("Please fill all the fields");
+    return;
+  }
+
+  // Create post data object
+  const postData = { title, body, author };
+
+  // Post data to the local API and update display
+  postLocalApi(postData).then(() => {
+    // Clear form fields
+    document.getElementById("title").value = "";
+    quill.root.innerHTML = "";
+    document.getElementById("author").value = "";
+    displayData();
+  });
+};
+
+// Function to edit a post
+const editPost = postId => {
+  // Prompt user for new title
+  const newTitle = prompt("Enter the new title:");
+
+  // Check if the title prompt was not canceled
+  if (newTitle !== null) {
+    // Prompt user for new author and body
+    const newAuthor = prompt("Enter the new author:");
+    const newBody = prompt("Enter the new body:");
+
+    // Check if both author and body prompts were not canceled
+    if (newAuthor !== null && newBody !== null) {
+      // Fetch API to update the post
+      fetch(`${apiUrl}/${postId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: newTitle, author: newAuthor, body: newBody })
+      })
+        .then(response => response.json())
+        .then(() => displayData());
     }
+  }
+};
 
-    const postData = {
-        title,
-        body,
-        author
-    }
+// Function to delete a post
+const deletePost = postId => {
+  // Confirm deletion with the user
+  const confirmDelete = confirm("Are you sure you want to delete this post?");
+  if (confirmDelete) {
+    // Fetch API to delete the post and update display
+    fetch(`${apiUrl}/${postId}`, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(() => displayData());
+  }
+};
 
-    postLocalApi(postData)
-        .then(() => {
-            // clear the form
-            document.getElementById("title").value = "";
-            document.getElementById("editor").value = "";
-            document.getElementById("author").value = "";
-
-            displayData();
-        });
-}
-
-// Edit post
-const editPost = (postId) => {
-    const newTitle = prompt("Enter the new title:");
-    if (newTitle !== null) {
-        fetch(`${apiUrl}/${postId}`, {
-            // fetch(apiUrl+"postId) {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ title: newTitle })
-        })
-            .then(response => response.json())
-            .then(() => displayData());
-    }
-}
-
-// Delete post
-const deletePost = (postId) => {
-    const confirmDelete = confirm("Are you sure you want to delete this post?");
-    if (confirmDelete) {
-        fetch(`${apiUrl}/${postId}`, {
-            method: 'DELETE'
-        })
-            .then(response => response.json())
-            .then(() => displayData());
-    }
-}
-
+// Initial display of posts on page load
 displayData();
 
-// event trigger for submit
+// Event listener for form submission
 document.getElementById("submit").addEventListener("click", submitData);
